acf9c04e841ab70f972a6d5cfc6ba7cf719306a2
Giyu2 <derdene719@gmail.com>
2025-02-25 15:53:49 +0800

    dfed

--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ import cors from 'cors'
 const app = express()
 const port = 4000;
 app.use(cors())
-
-
 app.use(express.json())
 
 
 
  
 app.use('/', (req, res)=> {
-    res.send("gg")
+    res.send("This is the test")
 });
 
  
  
 app.listen(port, () => {
-    console.log(`Example app listening on port
-        http://localhost:${port}`);
+    console.log(`Example app listening on port  ${port}`);
 })
